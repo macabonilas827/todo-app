6ddbcbffa5fc304d67931e29cb8e7392bba722d4
Mark Anel Cabonilas <macabonilas827@gmail.com>
2023-08-30 14:18:49 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="bg-mobile-light h-screen">
+      {/* <div className="">
+        <img
+          src="src/assets/images/bg-mobile-light.jpg"
+          alt="background light theme"
+        />
+      </div> */}
+    </div>
+  )
+}
+
+export default App
