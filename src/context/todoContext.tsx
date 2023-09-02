7ce7bd41c7ad3545dcfc3793d0276a01b51298a6
Mark Anel Cabonilas <macabonilas827@gmail.com>
2023-09-02 11:38:57 +0800

    add context api and reducer for the todos state

---
+++ src/context/todoContext.tsx
@@ -0,0 +1,38 @@
+import { Dispatch, ReactNode, createContext, useReducer } from 'react'
+
+type TodoAction = { type: 'ADD_TODO'; payload: Todo }
+
+interface Todo {
+  todo: string
+  todoId: string
+  isCompleted: boolean
+}
+interface TodoContextType {
+  todos: Todo[]
+  dispatch: Dispatch<TodoAction>
+}
+
+export const TodoContext = createContext<TodoContextType | undefined>(undefined)
+
+interface TodoProviderProps {
+  children: ReactNode
+}
+
+const todoReducer = (state: Todo[], action: TodoAction): Todo[] => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [...state, action.payload]
+    default:
+      return state
+  }
+}
+
+export const TodoProdiver = ({ children }: TodoProviderProps) => {
+  const [todos, dispatch] = useReducer(todoReducer, [])
+
+  return (
+    <TodoContext.Provider value={{ todos, dispatch }}>
+      {children}
+    </TodoContext.Provider>
+  )
+}
